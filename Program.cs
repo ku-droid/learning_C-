3e2711cd72733367458a588152a9a84a6ecb327b
ku-droid <52478527+ku-droid@users.noreply.github.com>
2022-11-08 15:31:21 +0545

    input and output of string and integers

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// NEW TO C#.
+// CREATING SIMPLE CONSOLE APP TO ENTER VALUE AND PRINT THE VALUE
+Console.WriteLine("enter the string\n");
+string a = Console.ReadLine();
+Console.WriteLine(a);
+Console.WriteLine("enter the number in integer\n");
+int b =Convert.ToInt32( Console.ReadLine()); 
+Console.WriteLine(b);
